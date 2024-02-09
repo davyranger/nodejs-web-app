11da3eacb63f92287b67203868d3fafc07a0e602
Davy <davy.strain@hotmail.com>
2024-02-09 17:46:58 +0800

    changes made to jenkins user data script

--- Terraform/jenkins-server/user_data.sh
+++ Terraform/jenkins-server/user_data.sh
@@ -11,7 +11,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo dnf install java-11-amazon-corretto -y
+sudo yum dnf install java-11-amazon-corretto -y
 sudo yum install jenkins -y
 
 
