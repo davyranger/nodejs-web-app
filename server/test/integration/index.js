6e81f30735188e4db6ab86322d5516602d26b83b
Davy <davy.strain@hotmail.com>
2024-02-11 14:29:05 +0800

    ameneded index.js

--- server/test/integration/index.js
+++ server/test/integration/index.js
@@ -1,9 +1,9 @@
-import chai, { expect } from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
 import app from "../../src/index.js";
 
 chai.use(chaiHttp);
-chai.should();
+const { expect } = chai;
 
 describe("GET /users", () => {
   it("Should display a list of users, fetched from db", (done) => {
@@ -11,9 +11,13 @@ describe("GET /users", () => {
       .request(app)
       .get("/users")
       .then((res) => {
-        expect(res).to.be.html;
-        res.text.should.match(/<h3.*>Rapidcode Users<\/h3>/g); // Updated to match <h3> tag
-        res.text.should.match(/<h3.*>.*<\/h3>/g); // Expecting <h3> tags for users
+        expect(res).to.have.status(200); // Ensure successful response
+        expect(res).to.be.html; // Ensure response is HTML
+        // Verify the presence of individual users in the response
+        expect(res.text).to.include("<h3>Fibre Bundle</h3>");
+        expect(res.text).to.include("<h3>Quantum Electrodynamics</h3>");
+        expect(res.text).to.include("<h3>Feynman Diagram</h3>");
+        expect(res.text).to.include("<h3>Differentiable Manifold</h3>");
         done();
       })
       .catch((err) => {
